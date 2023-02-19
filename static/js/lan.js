6f3244466de9fcee90c3b999f65dca0db2a6c37d
JamesCurtis <jamescurtis@foxmail.com>
2023-02-19 09:13:52 +0800

    init

---
+++ static/js/lan.js
@@ -0,0 +1,34 @@
+let g_mylan = {
+    cn:{
+        isrecover:'是否继续上次编辑?',
+        toolbar:'打开',
+        help:'关于',
+        toolbar_desc:'从本地选择',
+        toolbar_saveas:'另存为',
+        toolbar_new:'新建',
+        toolbar_op:'操作',
+        toolbar_clear:'清除',
+        toolbar_addmaplayser:'添加图层',
+        toolbar_zoomtofeatures:'缩放至要素',
+        toolbar_randompoint:'添加随机点',
+        toolbar_addbboex:'添加bboxes'
+    },
+    en:{
+        isrecover:'recover your map from the last time you edited?',
+        toolbar:'Open',
+        help:'关于',
+        toolbar_desc:'从本地选择',
+        toolbar_saveas:'另存为',
+        toolbar_new:'新建',
+        toolbar_op:'操作',
+        toolbar_clear:'清除',
+        toolbar_addmaplayser:'Add map layer',
+        toolbar_zoomtofeatures:'Zoom to features',
+        toolbar_randompoint:'添加随机点',
+        toolbar_addbboex:'Add bboxes'
+    }
+};
+
+let g_curlen = 'cn';
+
+g_mylan[g_curlen];
